21ea4e317bc2a6efa76e21dfec740845257df7e6
ulmastersproject <ulmastersproject@gmail.com>
2013-07-31 19:06:38 +0000

    Deployed Django App Live, made DEBUG=False

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@
 import os
 DIRNAME = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -25,7 +25,9 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.3/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+         '.pythonanywhere.com',
+    ]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
