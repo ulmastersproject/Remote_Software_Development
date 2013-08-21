9c986b96ac4f71b7e5d0996d7fce8b289076134e
ulmastersproject <ulmastersproject@gmail.com>
2013-08-21 11:29:15 +0100

    Update forms.py

--- Development/forms.py
+++ Development/forms.py
@@ -30,18 +30,3 @@ class DevelopmentForm(forms.Form):
         if num_words < 4:
             raise forms.ValidationError("Not enough code!")
         return message
-        
-# function that returns a list of the numbers of the Fibonacci series
-
-def fib2(n): # return Fibonacci series up to n
-    """Return a list containing the Fibonacci series up to n."""
-    result = []
-    a, b = 0, 1
-    while b < n:
-        result.append(b)    # see below
-        a, b = b, a+b
-    return result
-
-#=================================== 
-f100 = fib2(100)    # call it
-print f100          # write the result
